98efac0f09f3713a03436c0ae477bf4170c6a9fd
ford <tewarad2020>
2021-01-12 15:31:33 +0700

    code completed

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,14 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+int main () {
+    srand(time(0));
+    string grade[9] = {"A","B+","B","C+","C","D+","D","F","W"};
+    cout << "Press Enter 3 times to reveal your future.";
+    for (int i=0;i<3;i++) cin.get();
+    cout << "You will get " << grade[rand() % 9] << " in this 261102.";
+}
